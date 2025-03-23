1542d433254f69db9d8adaae6e8f8909bb0afd99
Evgeniy Bukharev <idksoda@mail.ru>
2025-03-23 21:43:15 +0300

    polish1

--- programs/genome-solana-contract/src/instructions/register_tournament.rs
+++ programs/genome-solana-contract/src/instructions/register_tournament.rs
@@ -14,7 +14,7 @@ use crate::{
 };
 
 pub fn handle_register_tournament(
-    ctx: Context<RegisterParticipantToTournamentSinglechain>,
+    ctx: Context<RegisterParticipant>,
     register_params: RegisterParams,
 ) -> Result<()> {
     let tournament = &mut ctx.accounts.tournament;
@@ -72,14 +72,10 @@ pub fn handle_register_tournament(
 }
 
 fn bloom_check(register_params: &RegisterParams, bloom: &mut Bloom) -> Result<()> {
-    if !bloom.insert(register_params.participant) {
-        return Err(TournamentError::AlreadyRegistered.into());
-    }
+    require!(bloom.insert(register_params.participant), TournamentError::AlreadyRegistered);
 
     for teammate in &register_params.teammates {
-        if !bloom.insert(teammate) {
-            return Err(TournamentError::AlreadyRegistered.into());
-        }
+        require!(bloom.insert(teammate), TournamentError::AlreadyRegistered);
     }
 
     Ok(())
@@ -87,17 +83,17 @@ fn bloom_check(register_params: &RegisterParams, bloom: &mut Bloom) -> Result<()
 
 #[derive(Accounts)]
 #[instruction(register_params: RegisterParams)]
-pub struct RegisterParticipantToTournamentSinglechain<'info> {
+pub struct RegisterParticipant<'info> {
     #[account(mut)]
-    pub participant: Signer<'info>,
+    participant: Signer<'info>,
     #[account(mut, seeds = [GENOME_ROOT, CONFIG], bump)]
-    pub config: Account<'info, GenomeConfig>,
+    config: Account<'info, GenomeConfig>,
     #[account(
         mut,
         seeds = [GENOME_ROOT, TOURNAMENT, register_params.tournament_id.to_le_bytes().as_ref()],
         bump
     )]
-    pub tournament: Account<'info, Tournament>,
+    tournament: Account<'info, Tournament>,
     #[account(
         init_if_needed,
         payer = participant,
@@ -105,40 +101,40 @@ pub struct RegisterParticipantToTournamentSinglechain<'info> {
         seeds = [GENOME_ROOT, TEAM, register_params.tournament_id.to_le_bytes().as_ref(), register_params.captain.as_ref()],
         bump
     )]
-    pub team: Account<'info, Team>,
+    team: Account<'info, Team>,
     #[account(address = tournament.asset_mint @ TournamentError::InvalidToken)]
-    pub mint: InterfaceAccount<'info, Mint>,
+    mint: InterfaceAccount<'info, Mint>,
     #[account(
         mut,
         associated_token::mint = mint,
         associated_token::authority = participant,
         associated_token::token_program = token_program,
     )]
-    pub participant_ata: InterfaceAccount<'info, TokenAccount>,
+    participant_ata: InterfaceAccount<'info, TokenAccount>,
     #[account(
         mut,
         associated_token::mint = mint,
         associated_token::authority = tournament,
         associated_token::token_program = token_program,
     )]
-    pub reward_pool_ata: Box<InterfaceAccount<'info, TokenAccount>>,
+    reward_pool_ata: Box<InterfaceAccount<'info, TokenAccount>>,
     #[account(mut, seeds = [GENOME_ROOT, BLOOM, register_params.tournament_id.to_le_bytes().as_ref()], bump)]
-    pub bloom_filter: Box<Account<'info, BloomFilter>>,
-    pub system_program: Program<'info, System>,
-    pub token_program: Interface<'info, TokenInterface>,
+    bloom_filter: Box<Account<'info, BloomFilter>>,
+    system_program: Program<'info, System>,
+    token_program: Interface<'info, TokenInterface>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct RegisterParams {
-    pub tournament_id: u32,
-    pub participant: Pubkey,
-    pub captain: Pubkey,
-    pub teammates: Vec<Pubkey>,
+    tournament_id: u32,
+    participant: Pubkey,
+    captain: Pubkey,
+    teammates: Vec<Pubkey>,
 }
 
 #[derive(Debug)]
 #[event]
 pub struct ParticipantRegistered {
-    pub participant: Pubkey,
-    pub captain: Pubkey,
+    participant: Pubkey,
+    captain: Pubkey,
 }
