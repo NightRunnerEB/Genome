81baa625468b9778ba7469bf81ab730541d890fa
Evgeniy Bukharev <idksoda@mail.ru>
2025-03-23 22:25:56 +0300

    polish9

--- programs/genome-solana-contract/src/instructions/set_bloom_precision.rs
+++ programs/genome-solana-contract/src/instructions/set_bloom_precision.rs
@@ -1,6 +1,9 @@
 use crate::{data::GenomeConfig, TournamentError, CONFIG, DEPLOYER, GENOME_ROOT};
 use anchor_lang::prelude::*;
 
+const MIN_PRECISION: f64 = 0.0;
+const MAX_PRECISION: f64 = 1.0;
+
 #[derive(Accounts)]
 pub struct SetBloomPrecision<'info> {
     #[account(mut, address = DEPLOYER @ TournamentError::InvalidAdmin)]
@@ -14,7 +17,7 @@ pub fn handle_set_bloom_precision(
     new_precision: f64,
 ) -> Result<()> {
     require!(
-        new_precision > 0.0 && new_precision < 1.0,
+        new_precision > MIN_PRECISION && new_precision < MAX_PRECISION,
         TournamentError::InvalidPrecision
     );
 
